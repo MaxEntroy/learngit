f0a4d47170ce8ccfc61938d061e3965c55653b0a
kang <likang@tju.edu.cn>
2018-06-20 15:00:20 +0800

    basic_print() is finished.

---
+++ main.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: main.c
+    > Author: kang
+    > Mail: likang@tju.edu.cn
+    > Created Time: 2018年06月20日 星期三 14时58分12秒
+ ************************************************************************/
+#include <stdio.h>
+
+void basic_print() {
+    printf("basic_print called.");
+}
+
+int main( void ) {
+    return 0;
+}
